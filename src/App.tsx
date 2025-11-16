c554ef081cfbd48c69343f582a178f057d71e6c8
Nitin Rajput <ntn.rajput89@gmail.com>
2025-11-16 23:04:38 +0530

    healers physiotherapy website new

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 
-import ScrollToTop from "./src/components/ScrollToTop";
+import ScrollToTop from "./components/ScrollToTop";
 import Index from "./pages/Index";
 import AboutUs from "./pages/AboutUs";
 import Services from "./pages/Services";
